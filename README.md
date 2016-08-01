e2b5b4a43e24d88e67f5937c156024a1aceb79b9
Guilherme Martins <adm.gms@gmail.com>
2016-08-01 09:36:59 -0300

    .

---
+++ README.md
@@ -0,0 +1,5 @@
+#Stars Background
+* the stars are positioned according to the screen resolution
+* I hope this will be useful for someone
+
+special thanks to: Matheus Freitas
---
+++ js/stars.js
@@ -0,0 +1,23 @@
+function blinker() {
+  $('.star').each(function(index, item) {
+    var Speed = Math.floor(Math.random() * 2000) + 1000;
+    $(item).fadeOut(Speed).fadeIn(Speed);
+  });
+  setTimeout(blinker, 500);
+}
+
+blinker();
+
+$(function() {
+  $('.star').each(function(index, item) {
+    var max_width  = $(window).width();
+    var max_height = $(window).height();
+    var starPosX    = Math.floor(Math.random() * max_width) + 1;
+    var starPosY    = Math.floor(Math.random() * max_height) + 1;
+    $(item).css({
+      left: starPosX -8,
+      top : starPosY -8
+    });
+    console.log(starPosX)
+  })
+});
